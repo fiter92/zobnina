5371b96fa99576de66b4790e1ebb73f4bd1f56a0
Andrei Zobnin <fiter92@gmail.com>
2017-11-04 01:28:05 +0300

    Begin

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-vidth,initoal-scale=1" >
+	<title>Актёрское агенство Зобниной Кристины</title>
+	<link rel="stylesheet" type="text/css" href="/css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="/css/style.css" />
+</head>
+<body>
+
+	<div class="wrapper">
+		
+		<div class="content">
+			
+			<header></header>
+
+			<section class="section-1">
+				
+				<div class="items">
+
+					<div class="item">
+						
+						<div class="gallery">
+							
+						</div>
+
+						<div class="panel">
+
+							<p>
+								<i class="fa fa-mars" aria-hidden="true"></i>
+								<span>М</span>
+							</p>
+
+							<p>
+								<i class="fa fa-arrows-v" aria-hidden="true"></i>
+								<span>165см</span>
+							</p>
+
+							<p>
+								<i class="fa fa-clock-o" aria-hidden="true"></i>
+								25
+							</p>
+
+							<p>
+								<i class="fa fa-arrows-v" aria-hidden="true"></i>
+								<span>165см</span>
+							</p>
+							
+							<p>
+								<i class="fa fa-info" aria-hidden="true"></i>
+							</p>
+
+						</div>
+						
+					</div>
+
+				</div>
+
+			</section>
+			
+		</div>
+	
+		<footer></footer>
+
+	</div>
+	
+</body>
+</html>
